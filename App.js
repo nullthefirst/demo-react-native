8489d1c0b4526eb4b20e87598ad3c125a7a2f24f
Usheninte <jrandangana@gmail.com>
2019-02-07 19:23:35 +0100

    used props in native

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-export default class NameList extends Component {
+class Greeting extends Component {
   render() {
     return(
-      <View>
-        <Text></Text>
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello from { this.props.name }!</Text>
+      </View>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return(
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Usheninte' />
+        <Greeting name='Joshua' />
+        <Greeting name='Dangana' />
       </View>
     );
   }
