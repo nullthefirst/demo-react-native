6e5f6c91d40d50f3a35871813fde766b3d316e01
Usheninte <jrandangana@gmail.com>
2019-02-07 16:32:35 +0100

    edited file

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default class HelloWorld extends Component {
   render() {
     return(
       <View>
-        <Text style={styles.greeting}>Hello World</Text>
+        <Text>Hello World</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  greeting: {
-    margin: 5,
-    color: "#212121"
-  }
-})
