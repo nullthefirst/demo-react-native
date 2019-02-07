602dae3d9ee92788a59cd5029cf78e9a3f9c19b4
Usheninte <jrandangana@gmail.com>
2019-02-07 16:39:01 +0100

    rebuilt App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react-native';
+import { Text, View } from 'react-native';
+
+export default class Learn2 extends Component {
+  render() {
+    return(
+      <View>
+        <Text></Text>
+      </View>
+    );
+  }
+}
