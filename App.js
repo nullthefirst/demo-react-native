9551fc6e3f4f1bfeb6128128b35a6b07adbdd3c9
Usheninte <jrandangana@gmail.com>
2019-02-09 19:35:05 +0100

    learning flex direction basics

--- App.js
+++ App.js
@@ -4,7 +4,11 @@ import { View } from 'react-native';
 export default class FlexDirectionBasics extends Component {
   render() {
     return(
-      <View style={{ flex: 1, flexDirection: 'column' }}>
+      <View style={{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center'
+      }}>
         <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
         <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
         <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
