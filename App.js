352ce051e881230a3478876d8a407c83a39d3fb4
Usheninte <jrandangana@gmail.com>
2019-02-09 19:46:49 +0100

    learning flex direction basics

--- App.js
+++ App.js
@@ -10,8 +10,8 @@ export default class FlexDirectionBasics extends Component {
         justifyContent: 'center'
       }}>
         <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
-        <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
-        <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
+        <View style={{ height: 50, backgroundColor: 'skyblue' }} />
+        <View style={{ height: 50, backgroundColor: 'steelblue' }} />
       </View>
     );
   }
