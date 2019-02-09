6a6d0c09b893f08a81d0944f03a69cf800b33f62
Usheninte <jrandangana@gmail.com>
2019-02-09 19:22:42 +0100

    learning dimesions basics

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import { View } from 'react-native';
 export default class FixedDimensionsBasics extends Component {
   render() {
     return(
-      <View>
-        <View style={{ backgroundColor: 'powderblue' }} />
-        <View style={{ backgroundColor: 'skyblue' }} />
-        <View style={{ backgroundColor: 'steelblue' }} />
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, backgroundColor: 'powderblue' }} />
+        <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
+        <View style={{ flex: 3, backgroundColor: 'steelblue' }} />
       </View>
     );
   }
