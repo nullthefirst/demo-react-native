d9a84a622bae835c7ce011b7befe4fc1ff55f84a
Usheninte <jrandangana@gmail.com>
2019-02-09 19:06:23 +0100

    learning dimesions basics

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react-native';
+import { View } from 'react-native';
+
+export default class FixedDimensionsBasics extends Component {
+  render() {
+    return(
+      <View></View>
+    );
+  }
+}
