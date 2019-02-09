72203998b8696a6a25fad3b1928e15ace6b0fc50
Usheninte <jrandangana@gmail.com>
2019-02-09 17:11:43 +0100

    building Blink component

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+class Blink extends Component {
+  render() {
+    return(
+      <View>
+        <Text></Text>
+      </View>
+    );
+  }
+}
+
 export default class Stateful extends Component {
   render() {
     return(
