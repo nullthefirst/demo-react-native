f596154af36d153e7a8c4536d6aa92deaa016234
Usheninte <jrandangana@gmail.com>
2019-02-09 19:26:32 +0100

    created new component

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-export default class FixedDimensionsBasics extends Component {
+export default class FlexDirectionBasics extends Component {
   render() {
     return(
       <View style={{ flex: 1 }}>
