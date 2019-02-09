2dc04bfc5564d580af19dc6b8ce4a13012b19932
Usheninte <jrandangana@gmail.com>
2019-02-09 19:10:47 +0100

    learning dimesions basics

--- App.js
+++ App.js
@@ -6,8 +6,8 @@ export default class FixedDimensionsBasics extends Component {
     return(
       <View>
         <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
-        {/* <View style={{ width: , height: , backgroundColor:  }} />
-        <View style={{ width: , height: , backgroundColor:  }} /> */}
+        <View style={{ width: , height: , backgroundColor:  }} />
+        {/* <View style={{ width: , height: , backgroundColor:  }} /> */}
       </View>
     );
   }
