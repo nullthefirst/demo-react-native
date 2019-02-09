f5bed61c7e38e0a19d87cd7ffc1618af6336ec9f
Usheninte <jrandangana@gmail.com>
2019-02-09 17:13:11 +0100

    building Blink component

--- App.js
+++ App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isShowingText: true };
+
+
+  }
+
   render() {
     return(
       <View>
