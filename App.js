8ebbfb75bddf55965792a89f935d8c3239aa028d
Usheninte <jrandangana@gmail.com>
2019-02-10 22:45:59 +0100

    building PizzaTranslator component

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { Text, TextInput, View } from 'react-native';
 
 export default class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
     return(
       <View style={{ padding: 40 }}>
