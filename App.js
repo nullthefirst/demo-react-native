f8bbadbd08badc7c1dfa32cf4e67c2606d91610d
Usheninte <jrandangana@gmail.com>
2019-02-10 22:34:38 +0100

    rebuilding App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, TextInput, View } from 'react-native';
+
+export default class PizzaTranslator extends Component {
+  render() {
+    return(
+      <View>
+
+      </View>
+    );
+  }
+}
