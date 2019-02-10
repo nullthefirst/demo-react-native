2aac65ac6067c67b7f4be47ca3af5030795118c2
Usheninte <jrandangana@gmail.com>
2019-02-11 00:49:46 +0100

    rebuilt App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return(
+      <View>
+        <Text style={{ padding: 20 }}>Hello World</Text>
+      </View>
+    );
+  }
+}
