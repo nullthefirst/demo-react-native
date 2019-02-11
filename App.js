c9b894deed8638436112b261e4365346e42b08d5
Usheninte <jrandangana@gmail.com>
2019-02-11 12:54:41 +0100

    building Touchables component

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { Alert, Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
 
 export default class Touchables extends Component {
+  onPressButton() {
+    Alert.alert('You tapped the button');
+  }
+
   render() {
     return(
       <View>
