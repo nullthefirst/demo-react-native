3d9806d0e5693cad072e491ca0def83aeca47626
Usheninte <jrandangana@gmail.com>
2019-02-11 12:53:43 +0100

    building Touchables component

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Alert, Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
+
+export default class Touchables extends Component {
+  render() {
+    return(
+      <View>
+        <Text></Text>
+      </View>
+    );
+  }
+}
